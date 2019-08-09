fab792e109900984454c8ce36283b95363233b5f
wuydit <wuydit@163.com>
2019-08-09 13:52:06 +0800

    添加注册中心验证

--- eureka-service/src/main/java/org/wuyd/acp/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/org/wuyd/acp/eurekaservice/EurekaServiceApplication.java
@@ -2,7 +2,9 @@ package org.wuyd.acp.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
 
