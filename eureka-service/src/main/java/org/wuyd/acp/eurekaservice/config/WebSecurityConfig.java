ebf11fdf2d76986b2a53299a5e42a22a3fb8ce7e
wuydit <wuydit@163.com>
2019-08-09 18:10:52 +0800

    user服务和认证服务

---
+++ eureka-service/src/main/java/org/wuyd/acp/eurekaservice/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package org.wuyd.acp.eurekaservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author wuyd
+ * 2019/8/9 14:37
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); //开启认证
+    }
+}
---
+++ user-service/src/main/java/org/wuyd/acp/userservice/controller/UserController.java
@@ -0,0 +1,9 @@
+package org.wuyd.acp.userservice.controller;
+
+/**
+ * @author wuyd
+ * 2019/8/9 17:59
+ */
+
+public class UserController {
+}
